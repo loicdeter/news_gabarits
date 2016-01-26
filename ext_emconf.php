80546f9a1f297da4e82de0fc29a0ec1d00de63fd
loicdeter <loic.deterwangne@gmail.com>
2016-01-26 11:27:01 +0100

    + add typo Configuration

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "news".
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'news_gabarits',
+	'description' => 'news_gabarits pour news',
+	'category' => 'fe',
+	'author' => 'Loïc de Terwangne',
+	'author_email' => 'support@cdgfpt46.fr',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 1,
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'bootstrap_package' => '6.2.8-6.99.99',
+		),
+		'conflicts' => array(),
+		'suggests' => array(),
+	),
+	'_md5_values_when_last_written' => '',
+	'suggests' => array(),
+);
