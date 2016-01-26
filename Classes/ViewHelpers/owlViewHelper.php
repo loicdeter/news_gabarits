0479d12a6fc73d75ac16b55bf9d22928712e2a35
loicdeter <loic.deterwangne@gmail.com>
2016-01-26 15:40:36 +0100

    * ViewHelper class corrections

--- Classes/ViewHelpers/owlViewHelper.php
+++ Classes/ViewHelpers/owlViewHelper.php
@@ -9,7 +9,7 @@ class owlViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper 
 	 */
 	
 	function render(){
-		$PageRenderer = t3lib_div::makeInstance('PageRenderer');
+		$PageRenderer = TYPO3\CMS\Core\Page\PageRenderer::makeInstance('PageRenderer');
 		
 		$file = "owl.starter.js";
 		$PageRenderer->addJsFooterFile($file);
