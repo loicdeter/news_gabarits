c629fb47bed1c7323179389942992aa0559223fa
loicdeter <loic.deterwangne@gmail.com>
2016-01-28 16:19:11 +0100

    * Update Typo3 configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,3 +2,17 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+
+/***************
+ * PageTS
+ */
+
+// Add tx_news.templateLayouts
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/tx_news.txt">');
+
+/***************
+ * Reset extConf array to avoid errors
+ */
+if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY])) {
+    $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY] = serialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+}
