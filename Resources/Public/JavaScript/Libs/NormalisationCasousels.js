f8b7db3434379788ddd9141968b4eac8ce1063dc
loicdeter <loic.deterwangne@gmail.com>
2016-02-02 16:51:20 +0100

    - FIX Javascript

--- Resources/Public/JavaScript/Libs/NormalisationCasousels.js
+++ Resources/Public/JavaScript/Libs/NormalisationCasousels.js
@@ -48,30 +48,30 @@ $(document).CarouselsHeight();
 
 $(document).ready(function(){
     $(div[id^="twbs-carousel-NewsCarousel-"]).each( function () {
-     var clickEvent = false;
-     $(this).on('click', '.list-group li', function() {
-   clickEvent = true;
-   $('.list-group li').removeClass('active');
-   $(this).addClass('active');  
-     }).on('slid.bs.carousel', function(e) {
-      if(!clickEvent) {
-   var count = $('.list-group').children().length -1;
-       var current = $('.list-group li.active');
-       current.removeClass('active').next().addClass('active');
-       var id = parseInt(current.data('slide-to'));
-       if(count == id) {
-        $('.list-group li').first().addClass('active'); 
-       }
-      }
-      clickEvent = false;
-     });
+        var clickEvent = false;
+            $(this).on('click', '.list-group li', function() {
+            clickEvent = true;
+            $('.list-group li').removeClass('active');
+            $(this).addClass('active');  
+        }).on('slid.bs.carousel', function(e) {
+            if(!clickEvent) {
+                var count = $('.list-group').children().length -1;
+                var current = $('.list-group li.active');
+                current.removeClass('active').next().addClass('active');
+                var id = parseInt(current.data('slide-to'));
+                if(count == id) {
+                    $('.list-group li').first().addClass('active'); 
+                }
+            }
+        clickEvent = false;
+        });
     });
 })
 $(window).load(function() {
-    $(div[id^="twbs-carousel-NewsCarousel-"]).each( function () {
+    $('div[id^="twbs-carousel-NewsCarousel-"]').each( function () {
         var boxheight = $(this).find('.carousel-inner').innerHeight();
         var itemlength = $(this).find('.item').length;
         var triggerheight = Math.round(boxheight/itemlength+1);
-     $(this).find('.list-group-item').outerHeight(triggerheight);
+        $(this).find('.list-group-item').outerHeight(triggerheight);
     });
 });
