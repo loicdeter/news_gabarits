6ca6fbf09d4d3f70451871c73fb6de7084b46fd7
loicdeter <loic.deterwangne@gmail.com>
2016-02-03 09:45:24 +0100

    - FIX javascript Tabbed Slider

--- Resources/Public/JavaScript/Libs/NormalisationCasousels.js
+++ Resources/Public/JavaScript/Libs/NormalisationCasousels.js
@@ -78,20 +78,23 @@ $(window).load(function() {
 });
 
 $(document).ready(function(){
-    $('div[id^="twbs-carousel-TabbedSlider-"').on('click', '.nav a', function() {
-        clickEvent = true;
-        $('.nav li').removeClass('active');
-        $(this).parent().addClass('active');
-    }).on('slid.bs.carousel', function(e) {
-        if(!clickEvent) {
-            var count = $('.nav').children().length -1;
-            var current = $('.nav li.active');
-            current.removeClass('active').next().addClass('active');
-            var id = parseInt(current.data('slide-to'));
-            if(count == id) {
-                $('.nav li').first().addClass('active');	
+    $('div[id^="twbs-carousel-TabbedSlider-"]').each( function () {
+        var clickEvent = false;
+        $(this).on('click', '.nav a', function() {
+            clickEvent = true;
+            $('.nav li').removeClass('active');
+            $(this).parent().addClass('active');
+        }).on('slid.bs.carousel', function(e) {
+            if(!clickEvent) {
+                var count = $('.nav').children().length -1;
+                var current = $('.nav li.active');
+                current.removeClass('active').next().addClass('active');
+                var id = parseInt(current.data('slide-to'));
+                if(count == id) {
+                    $('.nav li').first().addClass('active');	
+                }
             }
-        }
-        clickEvent = false;
+            clickEvent = false;
+        });
     });
-}
+})   
